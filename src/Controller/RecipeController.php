0da3fdc0f31cf3b35ed98b5ba6d7d10270ce44d8
Vitalii Stepaniuk <vitalii.stepaniuk@gmail.com>
2019-01-04 17:19:12 +0200

    Let's move regular way, api approach can wait for now.

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,19 +2,89 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use App\Form\RecipeType;
+use App\Repository\RecipeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
-class RecipeController extends ApiController
+/**
+ * @Route("/recipe")
+ */
+class RecipeController extends AbstractController
 {
     /**
-     * @Route("/recipe", name="recipe")
+     * @Route("/", name="recipe_index", methods={"GET"})
      */
-    public function index()
+    public function index(RecipeRepository $recipeRepository): Response
     {
-        return $this->respond([
-            'title' => 'Guess what?',
-            'another_attribute' => 2,
+        return $this->render('recipe/index.html.twig', ['recipes' => $recipeRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="recipe_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($recipe);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('recipe_index');
+        }
+
+        return $this->render('recipe/new.html.twig', [
+            'recipe' => $recipe,
+            'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/{id}", name="recipe_show", methods={"GET"})
+     */
+    public function show(Recipe $recipe): Response
+    {
+        return $this->render('recipe/show.html.twig', ['recipe' => $recipe]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="recipe_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Recipe $recipe): Response
+    {
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('recipe_index', ['id' => $recipe->getId()]);
+        }
+
+        return $this->render('recipe/edit.html.twig', [
+            'recipe' => $recipe,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="recipe_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Recipe $recipe): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$recipe->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($recipe);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('recipe_index');
     }
 }
