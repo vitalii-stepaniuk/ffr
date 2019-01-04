17e0e567489cce0a2836b57b1ce64aadcb24e552
Vitalii Stepaniuk <vitalii.stepaniuk@gmail.com>
2019-01-04 18:33:57 +0200

    added __toString() supermethods to condition entity classes.

--- src/Entity/WaterTemperatureCondition.php
+++ src/Entity/WaterTemperatureCondition.php
@@ -33,6 +33,11 @@ class WaterTemperatureCondition
         $this->recipes = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->title;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
