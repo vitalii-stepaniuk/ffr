33310e88ff2cdb9df4a6ee9798353908a46839ba
Vitalii Stepaniuk <vitalii.stepaniuk@gmail.com>
2019-02-07 23:37:22 +0200

    Added image fiedl to Brand entity as a string.

--- src/Entity/Brand.php
+++ src/Entity/Brand.php
@@ -36,6 +36,11 @@ class Brand
      */
     private $short_description;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -85,6 +90,18 @@ class Brand
     public function setShortDescription(string $short_description): self
     {
         $this->short_description = $short_description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
 
         return $this;
     }
